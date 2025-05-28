a22adf11b5288452b29ea04f4405e95beed29d63
Mirna Hasan <mirnamhasan50@gmail.com>
2025-05-28 12:17:41 +0330

    firstst

---
+++ lib/core/class/crud.dart
@@ -0,0 +1,45 @@
+
+
+// مهمة هذا الكلاس التعامل مع الريكويستس سواء الريكويست الذي يتم ارساله 
+// الى الباك ايند او الريكويست المستقبل من الباك ايند 
+// Either : اذا الميثود بترجع نوعين نستخدم either خاصية البرمجة الوظيفية 
+import 'dart:convert';
+
+import 'package:bloomy/core/class/statusrequest.dart';
+import 'package:bloomy/core/functions/checkinternet.dart';
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
+class Crud {
+ // هذه الميثود تتعامل مع الريكويست الذي يتم ارسالة الى او استقباله من باك ايند
+// و تقوم بفحص هل يوجد اتصال بالانترنت 
+
+Future<Either< StatusRequest , Map>> postData(String linkUrl , Map data) async{
+  
+ try{
+ if (await checkInternet()){
+    var response = await http.post(Uri.parse(linkUrl) ,body: data) ; 
+    if(response.statusCode == 200 || response.statusCode== 201){
+      Map responsebody = await jsonDecode(response.body);
+      return right(responsebody);
+    }
+    else {
+      return  left(StatusRequest.serverfailure);
+    }
+
+  }
+  else{
+    return left(StatusRequest.offlineFailure);
+
+  }
+
+
+
+ }
+catch(_){
+  return left(StatusRequest.serverfailure);
+}
+
+}
+
+
+}
