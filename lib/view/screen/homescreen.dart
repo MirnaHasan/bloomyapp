f3cec6c0f7e72bd75b294a4f5909e8470bd68393
Mirna Hasan <mirnamhasan50@gmail.com>
2025-06-27 00:14:17 +0330

    homescreenui

--- lib/view/screen/homescreen.dart
+++ lib/view/screen/homescreen.dart
@@ -10,13 +10,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     HomeComtrollerImp controller = Get.put(HomeComtrollerImp());
-    return Scaffold(body:  Center(
-      child: Column(
-        children: [
-          Center(child: Text("${controller.username}"),),
-        ],
-      ),
-    ),);
+    return Scaffold(
+      body:  Container(
+        child: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Row(children: [
+              Expanded(child: TextFormField(
+                
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search) ,
+                hintText:" Find products " , hintStyle:  TextStyle( fontSize: 18 ) ,
+                fillColor: Colors.grey[200],
+                filled: true,
+                border: OutlineInputBorder(
+                 borderSide: BorderSide.none,
+                 borderRadius: BorderRadius.circular(20) ,
+                
+                )
+              )
+
+              )) ,
+              SizedBox(width: 10 ) ,
+                Container(  decoration: BoxDecoration(    
+                  borderRadius:    BorderRadius.circular(10 ),
+                   color: Colors.grey[200],) ,
+                  width: 60 ,
+               
+                  padding: EdgeInsets.symmetric(vertical: 4) , 
+                  child: IconButton(onPressed: (){} , 
+            icon: Icon(Icons.notifications_active_outlined , size: 30  , color: Colors.grey[600] , )
+            
+            
+            ),),
+            ],),),
+          
+          ],
+        ),
+      )
+    );
    
   }
 }
